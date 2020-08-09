313192ac8be4fb11da25fe808e61b524cbfcf64a
graygabrielle <gray.atkinson.adams@gmail.com>
2020-08-09 21:43:02 +0200

    removed create react app, built draggable list in html, started styling in css

---
+++ script.js
@@ -0,0 +1,46 @@
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffet",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zuckerberg",
+  "Michael Bloomberg",
+  "Larry Page",
+];
+
+//store list items
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+//insert list items into DOM
+function createList() {
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+
+      listItem.setAttribute("data-index", index);
+
+      listItem.innerHTML = `
+    <span class="number">${
+      index + 1
+    }</span><div class="draggable" draggable="true">
+    <p class="person-name">${person}</p>
+    <i class="fas fa-grip-lines"></i></div>`;
+
+      listItems.push(listItem);
+
+      draggable_list.appendChild(listItem);
+    });
+}
